bbcd0ffee9b256aa2376b9b112aae0e6bcce6e55
Marco Berardini <mberardini@evolution-man.com>
2014-05-20 12:22:32 -0700

    Made change to routes file for Heroku deployment

--- Gemfile
+++ Gemfile
@@ -8,8 +8,9 @@ group :development, :test do
 	gem 'sqlite3'
 	gem 'rspec-rails', '2.13.1'
 	gem 'guard-rspec', '1.2.1'
-	gem 'guard-spork', '1.2.0'	
-	gem 'spork', '0.9.2'
+	gem 'guard-spork', '1.5.0'	
+	gem 'spork-rails', '4.0.0'
+	gem 'childprocess', '0.3.6'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
     
   end
@@ -43,5 +48,22 @@ describe "Static pages" do
     end
 
   end
+
+  describe "Contact" do
+
+    it "should have the content'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+
+  end
+
+
+
 
 end
